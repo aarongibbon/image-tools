b143e9e46b59a4c8f7eafea5fef4ed7e4d9bc232
Aaron Gibbon <aaron_gibbon@hotmail.co.uk>
2017-03-05 11:53:30 +0000

    Add videos and images subfolder, add test mode, refactor

--- image-organiser.sh
+++ image-organiser.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
+
+ROOTDIR="."
+
+while getopts d:t option
+do
+        case "${option}"
+        in
+                d) ROOTDIR=${OPTARG};;
+                t) TEST=true;;
+        esac
+done
+
 progress() {
 	echo -en "\r$1/$2 files processed..."
 }
 
-IMAGES=$(find $1 -regex ".*\.\(jpg\|gif\|png\|jpeg\)")
-OTHER=$(find $1 -not -regex ".*\.\(jpg\|gif\|png\|jpeg\)" | tail -n +2)
+cpfile() {	
+	if [ "$TEST" == true ]; then
+		cp --attributes-only $1 $2 >/dev/null 2>&1
+	else	
+		cp $1 $2 >/dev/null 2>&1
+	fi
+}
+
+IMAGES=$(find $ROOTDIR -regex ".*\.\(jpg\|gif\|png\|jpeg\)")
+VIDEOS=$(find $ROOTDIR -regex ".*\.\(mp4\)")
+OTHER=$(find $ROOTDIR -not -regex ".*\.\(jpg\|gif\|png\|jpeg\|mp4\)" | tail -n +2)
 
 #IMAGES_COUNT=${#IMAGES[@]}
 IMAGES_COUNT=$(wc -w <<< $IMAGES)
+VIDEO_COUNT=$(wc -w <<< $VIDEOS)
 OTHER_COUNT=$(wc -w <<< $OTHER)
 
-TOTAL_COUNT=$(($IMAGES_COUNT + $OTHER_COUNT))
+TOTAL_COUNT=$(($IMAGES_COUNT + $VIDEO_COUNT + $OTHER_COUNT))
 RUNNING_COUNT=0
-mkdir -p photos
-mkdir -p photos/misc
+
+ROOTFOLDER="media"
+IMAGEFOLDER="$ROOTFOLDER/images"
+VIDEOFOLDER="$ROOTFOLDER/videos"
+MISCFOLDER="$ROOTFOLDER/misc"
+
+mkdir -p $IMAGEFOLDER
+mkdir -p $VIDEOFOLDER
+mkdir -p $MISCFOLDER
 
 for i in $IMAGES
 do
@@ -24,19 +53,27 @@ do
 	then
 		MONTH=$(date -d $CREATEDATE '+%b')
 		YEAR=$(date -d $CREATEDATE '+%Y')
-		FOLDER="photos/$YEAR/$MONTH"
+		FOLDER="$IMAGEFOLDER/$YEAR/$MONTH"
 		mkdir -p $FOLDER
-		cp $i $FOLDER >/dev/null 2>&1
+		cpfile $i $FOLDER
 	else
-		cp $i "photos/misc" >/dev/null 2>&1
+		cpfile $i "$MISCFOLDER"
 	fi
 	((RUNNING_COUNT++))
 	progress $RUNNING_COUNT $TOTAL_COUNT
 done
 
+for i in $VIDEOS
+do
+	cpfile $i "$VIDEOFOLDER"
+	((RUNNING_COUNT++))
+	progress $RUNNING_COUNT $TOTAL_COUNT
+	
+done
+
 for i in $OTHER
 do
-	cp $i "photos/misc" >/dev/null 2>&1
+	cpfile $i "$MISCFOLDER"
 	((RUNNING_COUNT++))
 	progress $RUNNING_COUNT $TOTAL_COUNT
 done
